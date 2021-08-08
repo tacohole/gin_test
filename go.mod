48ab3a7a1dc53b85dd54b84bef50cc04227c19b6
Troy Coll <troycoll@Admins-MBP.nc.rr.com>
2021-08-08 08:46:27 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/gin_test
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.3 // indirect
---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+
+	r.Run()
+}
